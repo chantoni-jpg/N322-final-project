1e82f7a22043e07780b6728391f5fb250cdada8d
chantoni-jpg <78805182+chantoni-jpg@users.noreply.github.com>
2022-11-30 15:11:35 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'final.project',
+  appName: 'final-project',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
